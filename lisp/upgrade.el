2fef799c20a7cae0f62ad7ccd46ec5f2149f50c8
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-13 23:25:58 +0800

    Add upgrade

---
+++ lisp/upgrade.el
@@ -0,0 +1,8 @@
+;; ========================================================================
+;; $File: upgrade.el $
+;; $Date: 2022-03-13 23:25:30 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2022 by Shen, Jen-Chieh $
+;; ========================================================================
