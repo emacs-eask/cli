df5db3af4a626f00def617bf0d25a98b743b992c
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-13 21:45:58 +0800

    Add lisp

---
+++ lisp/install.el
@@ -0,0 +1,8 @@
+;; ========================================================================
+;; $File: install.el $
+;; $Date: 2022-03-13 21:37:28 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2022 by Shen, Jen-Chieh $
+;; ========================================================================
