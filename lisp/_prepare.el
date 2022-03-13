40e1c4d3aed861d4238f83ae48fcfe248ae7c69e
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-14 03:36:38 +0800

    Rename prepare

--- lisp/_prepare.el
+++ lisp/_prepare.el
@@ -8,6 +8,7 @@
 ;; ========================================================================
 
 (require 'package)
+(require 'subr-x)
 
 (defun eask-global-p ()
   "Return non-nil if the workspace is global."
@@ -17,9 +18,15 @@
 (defmacro eask-start (&rest body)
   "Execute BODY with workspace setup."
   (declare (indent 0) (debug t))
-  `(progn
-     (unless (eask-global-p)  ; set it locally, else we ignore to respect default settings
-       (setq user-emacs-directory (expand-file-name ".eask/")
-             package-user-dir (expand-file-name "elpa" user-emacs-directory)))
-     (ignore-errors (make-directory package-user-dir t))
+  ;; set it locally, else we ignore to respect default settings
+  `(if-let ((_ (not (eask-global-p)))
+            (user-emacs-directory (expand-file-name ".eask/"))
+            (package-user-dir (expand-file-name "elpa" user-emacs-directory))
+            (user-init-file (locate-user-emacs-file "init.el"))
+            (custom-file (locate-user-emacs-file "custom.el"))
+            (eask-file (expand-file-name "../Eask" user-emacs-directory)))
+       (progn
+         (ignore-errors (make-directory package-user-dir t))
+         (ignore-errors (load-file eask-file))
+         ,@body)
      ,@body))
