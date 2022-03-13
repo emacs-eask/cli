81503a7a38e222b745497b8782adb2b06e911bf6
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-14 03:36:22 +0800

    Add clean

---
+++ cmds/clean.js
@@ -0,0 +1,19 @@
+/**
+ * $File: clean.js $
+ * $Date: 2022-03-14 03:04:26 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2022 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+const util = require("../src/util");
+
+exports.command = "clean";
+exports.desc = "Clean up local .eask directory";
+
+exports.handler = async ({}) => {
+  await util.call('clean');
+};
