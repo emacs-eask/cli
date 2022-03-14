4f016495a248bbab3bb4bc792b3ad575e02140b2
Jen-Chieh <jcs090218@gmail.com>
2022-03-14 15:36:02 +0800

    Update upgrade

--- lisp/upgrade.el
+++ lisp/upgrade.el
@@ -16,13 +16,54 @@
 
 (load-file "./lisp/_prepare.el")
 
+(defun eask-package-version (name where)
+  "Get version of the package by NAME.
+
+Argument WHERE is the alist of package information."
+  (when-let ((pkg (cadr (assq name where))))
+    (package-desc-version pkg)))
+
+(defun eask-package-upgrade (pkg-desc)
+  "Upgrade package using PKG-DESC."
+  (let ((old-pkg (cadr (assq (package-desc-name pkg-desc) package-alist))))
+    (package-install pkg-desc)
+    (package-delete old-pkg)))
+
+(defun eask-package--upgradable-p (pkg)
+  "Return non-nil if PKG can be upgraded."
+  (let ((current (eask-package-version pkg package-alist))
+        (latest (eask-package-version pkg package-archive-contents)))
+    (version-list-< current latest)))
+
+(defun eask-package--upgrades ()
+  "Return a list of upgradable package description."
+  (let (upgrades)
+    (dolist (pkg (mapcar #'car package-alist))
+      (when (eask-package--upgradable-p pkg)
+        (push (cadr (assq pkg package-archive-contents)) upgrades)))
+    upgrades))
+
+(defun eask-package-upgrade-all ()
+  "Upgrade for archive packages."
+  (if-let ((upgrades (eask-package--upgrades)))
+      (when (yes-or-no-p
+             (format "Upgrade %d package%s (%s)? "
+                     (length upgrades)
+                     (if (= (length upgrades) 1) "" "s")
+                     (mapconcat #'package-desc-full-name upgrades ", ")))
+        (dolist (pkg-desc upgrades) (eask-package-upgrade pkg-desc))
+        (message "Done upgrading all packages"))
+    (message "All packages are up to date")))
+
 (eask-start
   (package-refresh-contents)
 
   (if-let* ((name (elt argv 0)) (name (intern name)))
       (if (package-installed-p name)
-          ()
-        (error ""))
-    ))
+          (if (eask-package--upgradable-p name)
+              (eask-package-upgrade name)
+            (message "Package [%s] is already up to date" name))
+        (error "Package does not exists [%s], you need to install before upgrade" name))
+    (eask-package-upgrade-all)))
 
 ;;; upgrade.el ends here
