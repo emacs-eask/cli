c1c25385d95a478ba659b995d80fd63fd0fa4d17
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-16 02:56:23 +0800

    Add init

---
+++ lisp/init.el
@@ -0,0 +1,20 @@
+;;; init.el --- Install packages  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; Create Eask file,
+;;
+;;   $ eask init
+;;
+
+;;; Code:
+
+(load-file (expand-file-name
+            "_prepare.el"
+            (file-name-directory (nth 1 (member "-scriptload" command-line-args)))))
+
+(eask-start
+  ;; TODO: ..
+  )
+
+;;; init.el ends here
