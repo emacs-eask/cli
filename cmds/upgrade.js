67a0af5546edd51f5a36e8f8f24f82719576f4c4
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-15 23:03:42 +0800

    Update header

--- cmds/upgrade.js
+++ cmds/upgrade.js
@@ -1,10 +1,20 @@
 /**
- * $File: upgrade.js $
- * $Date: 2022-03-13 22:27:11 $
- * $Revision: $
- * $Creator: Jen-Chieh Shen $
- * $Notice: See LICENSE.txt for modification and distribution information
- *                   Copyright © 2022 by Shen, Jen-Chieh $
+ * Copyright (C) 2022 Jen-Chieh Shen
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Emacs; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 "use strict";
