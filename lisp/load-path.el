da9339605612d25082ff80f5f9b01ee085e34725
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-17 02:33:40 +0800

    Done load-path

---
+++ lisp/load-path.el
@@ -0,0 +1,23 @@
+;;; load-path.el --- Print the load-path from workspace  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; Print the load-path from workspace
+;;
+;;   $ eask load-path
+;;
+
+;;; Code:
+
+(load-file (expand-file-name
+            "_prepare.el"
+            (file-name-directory (nth 1 (member "-scriptload" command-line-args)))))
+
+(defun eask--print-load-path (path)
+  "Print out the PATH."
+  (message "%s" path))
+
+(eask-start
+  (mapc #'eask--print-load-path load-path))
+
+;;; load-path.el ends here
