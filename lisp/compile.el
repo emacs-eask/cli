f2422276748cdba2dd74b33fcf741f491dbc65df
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-16 17:59:18 +0800

    Done compile

--- lisp/compile.el
+++ lisp/compile.el
@@ -13,14 +13,26 @@
             "_prepare.el"
             (file-name-directory (nth 1 (member "-scriptload" command-line-args)))))
 
+(defun eask--f-entries (path pattern)
+  "Return entries from PATH with PATTERN."
+  (when (file-directory-p path)
+    (cl-remove-if-not
+     (lambda (file) (string-match-p pattern file))
+     (directory-files-recursively path "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)"))))
+
+(defun eask--byte-compile-file (filename)
+  "Byte compile FILENAME with display messages."
+  (message "Compiling file... %s" filename)
+  (byte-compile-file filename)
+  (message "Done compile, %s" filename))
+
 (eask-start
-  ;; TODO: ..
-  (message ">> %s" eask-package-file)
-  (message ">> %s" package-archives)
-  (byte-compile-file eask-package-file)
+  (package-initialize)
+  (package-refresh-contents)
   (dolist (pattern eask-files)
-    (message "%s" (directory-files default-directory t pattern))
-    )
-  )
+    (dolist (filename (eask--f-entries default-directory pattern))
+      (add-to-list 'load-path (file-name-directory filename))
+      (eask--byte-compile-file filename)))
+  (eask--byte-compile-file eask-package-file))
 
 ;;; compile.el ends here
