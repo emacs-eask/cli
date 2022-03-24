a4f04b76c67a81215c2caa934606dc7aa87fe58e
Jen-Chieh <jcs090218@gmail.com>
2022-03-24 13:35:58 +0800

    Add command archives

---
+++ lisp/archives.el
@@ -0,0 +1,32 @@
+;;; archives.el --- List out all package archives  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; Command use to list out all package archives,
+;;
+;;   $ eask archives
+;;
+;;
+;;  Action options:
+;;
+;;    [-g]          install packages to default `~/.emacs.d/'
+;;
+
+;;; Code:
+
+(load-file (expand-file-name
+            "_prepare.el"
+            (file-name-directory (nth 1 (member "-scriptload" command-line-args)))))
+
+(defun eask--print-archive (archive)
+  "Print the archive."
+  (message "  %-15s %-40s" (car archive) (cdr archive)))
+
+(eask-start
+  (if package-archives
+      (progn
+        (message "")
+        (mapc #'eask--print-archive package-archives))
+    (message "\n No archive has been selected")))
+
+;;; archives.el ends here
