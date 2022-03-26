a859f3149e3cd02346f06cca9fdea90770e7c41b
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-26 17:07:20 +0800

    code: Specify build

--- lisp/build.el
+++ lisp/build.el
@@ -1,8 +1,23 @@
-;; ========================================================================
-;; $File: build.el $
-;; $Date: 2022-03-26 17:02:53 $
-;; $Revision: $
-;; $Creator: Jen-Chieh Shen $
-;; $Notice: See LICENSE.txt for modification and distribution information
-;;                   Copyright © 2022 by Shen, Jen-Chieh $
-;; ========================================================================
+;;; build.el --- Build package into installable tar file  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; Command use to build package into installable tar file
+;;
+;;   $ eask build
+;;
+
+;;; Code:
+
+(load-file (expand-file-name
+            "_prepare.el"
+            (file-name-directory (nth 1 (member "-scriptload" command-line-args)))))
+
+(eask-start
+  (eask-pkg-init)
+  (eask-package-install 'package-build)
+  (if (eask-package-multi-p)
+      ()
+    (message "\n No need to build single file packages")))
+
+;;; build.el ends here
