9221751019bd9556735dffe464a3ddb8e5e3d686
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-26 17:04:44 +0800

    code: new command build

---
+++ lisp/build.el
@@ -0,0 +1,8 @@
+;; ========================================================================
+;; $File: build.el $
+;; $Date: 2022-03-26 17:02:53 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2022 by Shen, Jen-Chieh $
+;; ========================================================================
