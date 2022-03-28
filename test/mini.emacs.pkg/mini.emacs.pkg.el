209e6f440b25c9f96743d4bf4597d62c0f65eff6
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-29 01:54:10 +0800

    ci:Add test pkg

---
+++ test/mini.emacs.pkg/mini.emacs.pkg.el
@@ -0,0 +1,39 @@
+;;; mini.emacs.pkg.el --- Minimal test package  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Created date 2022-03-29 01:52:58
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Minimal Emacs package to simulate development environment; only for testing purposes!
+;; Keyword: test
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/emacs-eask/mini.emacs.pkg
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Minimal Emacs package to simulate development environment; only for testing
+;; purposes!
+;;
+
+;;; Code:
+
+(require 'cl-lib)
+
+(provide 'mini.emacs.pkg)
+;;; mini.emacs.pkg.el ends here
