b928593a03eb7d08d8eb7535eed3eb40389367b9
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-29 17:18:17 +0800

    ci: Add commentary

--- test/development/test-redefine.el
+++ test/development/test-redefine.el
@@ -2,7 +2,10 @@
 
 ;;; Commentary:
 ;;
+;; Test the concatened file built from `eask concat`.
 ;;
+;; We are only going to check for rule `redefine', since this isn't a regular
+;; Emacs package.
 ;;
 
 ;;; Code:
@@ -11,16 +14,16 @@
 
 (let* ((concated-file "./dist/eask.built.el")
        (concated-file (expand-file-name concated-file))
-       (test-conditions "
+       (conditions "
 ;; Local Variables:
 ;; coding: utf-8
 ;; byte-compile-warnings: (redefine)
 ;; End:
 "))
   (with-current-buffer (find-file concated-file)
-    (unless (string-suffix-p test-conditions (buffer-string))
+    (unless (string-suffix-p conditions (buffer-string))
       (goto-char (point-max))
-      (insert test-conditions))
+      (insert conditions))
     (byte-compile-file buffer-file-name)))
 
 ;;; test-redefine.el ends here
