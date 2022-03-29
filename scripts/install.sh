4f03f31d6f04f5ebda5e4b87f3b07ad6c7c7b3c0
Jen-Chieh <jcs090218@gmail.com>
2022-03-29 14:57:05 +0800

    ci: udpate script

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,24 +17,11 @@
 # Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301, USA.
 
-# get the .bashrc path
-BASHRC_FILE=$HOME/.bashrc
+EASK_BIN=$PWD/bin
+set PATH=$PATH:EASK_BIN
 
-# setup the environment into .bashrc file.
-echo "adding path to .bashrc file..."
-cat <<EOF >> $BASHRC_FILE
-# eask install package
-EASK_BIN='$PWD/bin'
-export EASK_BIN
-export PATH=$PATH:$EASK_BIN
-EOF
-
-echo "EASK_BIN='$PWD/bin'"
-echo "export EASK_BIN"
-echo "export PATH=$PATH:$EASK_BIN"
-
-echo "reload .bashrc file..."
-source ~/.bashrc
+# See new PATH variable
+echo PATH
 
 # See if `eask` install successfully!
 eask --version
