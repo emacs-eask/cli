afd25d14e72b96d23e68c5d073fd01e0f89cf900
Jen-Chieh <jcs090218@gmail.com>
2022-03-29 14:46:31 +0800

    Add exec project

---
+++ test/commands/exec/exec.el
@@ -0,0 +1,38 @@
+;;; exec.el --- Test project for command exec  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Created date 2022-03-29 14:10:40
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Test project for command exec.
+;; Keyword: exec
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/eask/eask
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Test project for command exec.
+;;
+
+;;; Code:
+
+(require 'cl-lib)
+
+(provide 'exec)
+;;; exec.el ends here
