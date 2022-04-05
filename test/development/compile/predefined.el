6a579bf937ba0f1f41f0f9913a373e7f84e54c65
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-06 03:17:27 +0800

    ci: no longer need predefined

--- test/development/compile/predefined.el
+++
@@ -1,6 +0,0 @@
-(defvar package-build-archive-dir)
-(defvar commander-args)
-(defvar package-build-default-files-spec)
-
-(declare-function 'package-build--package "package-build.el")
-(declare-function 'package-directory-recipe "package-recipe.el")
