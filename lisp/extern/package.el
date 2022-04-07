2d7dfe416a5be2c5c10a524a84475cba0359fbc3
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-07 23:13:30 +0800

    extern: brought in activate all

--- lisp/extern/package.el
+++ lisp/extern/package.el
@@ -17,4 +17,26 @@
     (unless package-archive-contents
       (package-refresh-contents))))
 
+;;
+;;; Emacs 27.1
+(eask-defun-fbound 'package-activate-all
+  (defun package-activate-all ()
+    "Activate all installed packages.
+The variable `package-load-list' controls which packages to load."
+    (setq package--activated t)
+    (let* ((elc (concat package-quickstart-file "c"))
+           (qs (if (file-readable-p elc) elc
+                 (if (file-readable-p package-quickstart-file)
+                     package-quickstart-file))))
+      (if qs
+          ;; Skip load-source-file-function which would slow us down by a factor
+          ;; 2 when loading the .el file (this assumes we were careful to
+          ;; save this file so it doesn't need any decoding).
+          (let ((load-source-file-function nil))
+            (unless (boundp 'package-activated-list)
+              (setq package-activated-list nil))
+            (load qs nil 'nomessage))
+        (require 'package)
+        (package--activate-all)))))
+
 ;;; package.el ends here
