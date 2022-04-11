8647d37c80c5349d8b3b3b17b8570a91bb91339c
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-11 23:31:33 +0800

    code: new command ert-runner

---
+++ lisp/lint/ert-runner.el
@@ -0,0 +1,33 @@
+;;; ert-runner.el --- Run ert tests using ert-runner  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; Command to run ert tests using ert-runner,
+;;
+;;   $ eask ert-runner [files..]
+;;
+;;
+;;  Initialization options:
+;;
+;;    [files..]     specify files to run ert tests
+;;
+
+;;; Code:
+
+(load (expand-file-name
+       "../_prepare.el"
+       (file-name-directory (nth 1 (member "-scriptload" command-line-args))))
+      nil t)
+
+;; Handle options
+(add-hook 'eask-before-command-hook
+          (lambda ()
+            (when (= eask-verbosity 4) (setq ert-runner-verbose t))))
+
+(eask-start
+  (eask-pkg-init)
+  (eask-with-archives "melpa"
+    (eask-package-install 'ert-runner))
+  (require 'ert-runner))
+
+;;; ert-runner.el ends here
