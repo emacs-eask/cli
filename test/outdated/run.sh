86f8eb059bbc15c6e5212675cdb7eec60e30b0b4
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-12 03:13:06 +0800

    ci: add test for outdated command

---
+++ test/outdated/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2022 Jen-Chieh Shen
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+
+## Commentary:
+#
+# Test command `exec`
+#
+
+echo "Test command 'search'..."
+cd $(dirname "$0")
+
+eask search
+eask search company
+eask search company dash --depth 0
+eask search company dash f s --depth 0 -g
