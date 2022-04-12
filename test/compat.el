457cd2fdaf29fe0c010cd3aa8ebf92c875816464
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-12 19:49:37 +0800

    ci: complete test compat

--- test/compat.el
+++ test/compat.el
@@ -2,6 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
-()
+(defconst compat-functions
+  '(url-file-exists-p)
+  "List of function to check Emacs compatibility.")
+
+(dolist (func compat-functions)
+  (let ((exists-p))
+    (eask-with-progress
+      (format "Checked function %s... " func)
+      (setq exists-p (fboundp func))
+      (if exists-p "done ✓" "missing ✗"))
+    (unless exists-p
+      (kill-emacs 1))))
 
 ;;; compat.el ends here
