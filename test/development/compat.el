9de8bc97b1da0cc3fa9260fcc25af58548f05ac5
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-13 22:56:37 +0800

    ci: add var compat test

--- test/development/compat.el
+++ test/development/compat.el
@@ -3,7 +3,10 @@
 ;;; Code:
 
 (defconst compat-functions
-  '(url-file-exists-p)
+  '(url-file-exists-p
+    package--alist
+    package--activate-all
+    package-activate-all)
   "List of function to check Emacs compatibility.")
 
 (dolist (func compat-functions)
@@ -13,6 +16,19 @@
       (setq exists-p (fboundp func))
       (if exists-p "done ✓" "missing ✗"))
     (unless exists-p
-      (eask-error "Missing %s in Emacs version %s... " func emacs-version))))
+      (eask-error "Missing function %s in Emacs version %s... " func emacs-version))))
+
+(defconst compat-variables
+  '(package-quickstart-file)
+  "List of variables to check Emacs compatibility.")
+
+(dolist (var compat-variables)
+  (let ((exists-p))
+    (eask-with-progress
+      (format "Checked variable %s... " var)
+      (setq exists-p (boundp var))
+      (if exists-p "done ✓" "missing ✗"))
+    (unless exists-p
+      (eask-error "Missing variable %s in Emacs version %s... " var emacs-version))))
 
 ;;; compat.el ends here
