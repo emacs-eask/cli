621fb8eefe41f513c9fe090daaec24c1ee9083e6
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-17 18:39:50 +0800

    Add check-eask command

---
+++ lisp/lint/check-eask.el
@@ -0,0 +1,34 @@
+;;; check-eask.el --- Run eask checker  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; Commmand use to run Eask checker
+;;
+;;   $ eask check-eask
+;;
+
+;;; Code:
+
+(load (expand-file-name
+       "../_prepare.el"
+       (file-name-directory (nth 1 (member "-scriptload" command-line-args))))
+      nil t)
+
+(defvar eask--checker-log nil)
+
+(defun eask--write-log (level msg)
+  "Write the log."
+  (let* ((level (format "%s" level))
+         (log (format "%s:%s:%s %s: %s" (file-name-nondirectory load-file-name)
+                      (line-number-at-pos) (current-column)
+                      (capitalize level)
+                      msg)))
+    (push log eask--checker-log)))
+
+(add-hook 'eask-on-error-hook #'eask--write-log)
+(add-hook 'eask-on-warning-hook #'eask--write-log)
+
+(eask-start
+  (mapc #'eask-msg eask--checker-log))
+
+;;; check-eask.el ends here
