8d68ed89f19cff4647b9d778b7be0d1a2fb869bc
Jen-Chieh <jcs090218@gmail.com>
2022-04-18 15:09:34 +0800

    Limiit error from load

--- lisp/util/check-eask.el
+++ lisp/util/check-eask.el
@@ -23,15 +23,16 @@
 
 (defun eask--write-log (level msg)
   "Write the log."
-  (with-current-buffer (eask--load-buffer)
-    (let* ((level-string (cl-case level
-                           (`error "Error")
-                           (`warn  "Warning")))
-           (log (format "%s:%s:%s %s: %s" (file-name-nondirectory load-file-name)
-                        (line-number-at-pos) (current-column)
-                        level-string
-                        msg)))
-      (push (ansi-color-filter-apply log) eask--checker-log))))
+  (when (string= (buffer-name) " *load*")
+    (with-current-buffer (eask--load-buffer)
+      (let* ((level-string (cl-case level
+                             (`error "Error")
+                             (`warn  "Warning")))
+             (log (format "%s:%s:%s %s: %s" (file-name-nondirectory load-file-name)
+                          (line-number-at-pos) (current-column)
+                          level-string
+                          msg)))
+        (push (ansi-color-filter-apply log) eask--checker-log)))))
 
 (add-hook 'eask-on-error-hook #'eask--write-log)
 (add-hook 'eask-on-warning-hook #'eask--write-log)
