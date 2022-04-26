9fcc09219dee3213b151fe89a0f6abc83b6838d2
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-27 04:31:47 +0800

    include when-let

--- lisp/extern/subr-x.el
+++ lisp/extern/subr-x.el
@@ -8,6 +8,13 @@
 ;;; Code:
 
 (eask-defvc< 26
+  (defmacro when-let* (varlist &rest body)
+    "Bind variables according to VARLIST and conditionally evaluate BODY.
+This is like `when-let' but doesn't handle a VARLIST of the form
+\(SYMBOL SOMETHING) specially."
+    (declare (indent 1) (debug if-let*))
+    (list 'if-let* varlist (macroexp-progn body)))
+
   (defmacro if-let* (varlist then &rest else)
     "Bind variables according to VARLIST and evaluate THEN or ELSE.
 This is like `if-let' but doesn't handle a VARLIST of the form
