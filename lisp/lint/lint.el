9f50e7e9eedce8dd1e0accf0a0d04d83ed895437
Jen-Chieh <jcs090218@gmail.com>
2022-04-27 13:46:28 +0800

    code: improve lint

--- lisp/lint/lint.el
+++ lisp/lint/lint.el
@@ -38,20 +38,18 @@
 (eask-start
   (eask-with-archives "melpa"
     (eask-package-install 'package-lint))
-  (let* ((given-args (eask-args))
-         (files (if given-args (eask-expand-file-specs given-args)
-                  (eask-package-el-files))))
-    (if files
+  (if-let ((files (if (eask-args) (eask-expand-file-specs (eask-args))
+                    (eask-package-el-files))))
+      (progn
+        (setq package-lint-main-file eask-package-file)
+        (mapcar #'eask--package-lint-file files)
+        (eask-info "(Total of %s files linted)" (length files)))
+    (eask-info "(No files have been linted)")
+    (if (eask-args)
         (progn
-          (setq package-lint-main-file eask-package-file)
-          (mapcar #'eask--package-lint-file files)
-          (eask-info "(Total of %s files linted)" (length files)))
-      (eask-info "(No files have been linted)")
-      (if given-args
-          (progn
-            (eask-log "")
-            (eask-log "Cannot find matching files with given pattern %s" (eask-args))
-            (eask-log ""))
-        (eask-help 'lint)))))
+          (eask-log "")
+          (eask-log "Cannot find matching files with given pattern %s" (eask-args))
+          (eask-log ""))
+      (eask-help 'lint))))
 
 ;;; lint.el ends here
