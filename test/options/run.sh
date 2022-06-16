0ba9a7ebee7061d1dc8cfe4b0aeab0b68d0d3798
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-16 17:20:46 +0800

    test(options): ci

---
+++ test/options/run.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2022 Jen-Chieh Shen
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+
+## Commentary:
+#
+# Test all options flag
+#
+
+echo "Test all options flag"
+cd $(dirname "$0")
+
+# Please have these flags in order, see `eask` file in the project root!
+eask info --g
+eask info --global
+eask info --f
+eask info --force
+eask info --dev
+eask info --development
+eask info --debug
+eask info --strict
+eask info --allow-error
+eask info --insecure
+eask info --timestamps
+eask info --log-level
+eask info --elapsed-time
+eask info --et
+eask info --no-color
+eask info --proxy localhost:8080
+eask info --http-proxy localhost:8080
+eask info --https-proxy localhost:8080
+eask info --no-proxy localhost:8080
+eask info --verbose 4
