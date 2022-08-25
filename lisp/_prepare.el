080bef053a5675f45e7b6f698404e0c62bdad0f8
Jen-Chieh <jcs090218@gmail.com>
2022-08-25 13:43:46 +0800

    add more log files

--- lisp/_prepare.el
+++ lisp/_prepare.el
@@ -990,16 +990,20 @@ Standard is, 0 (error), 1 (warning), 2 (info), 3 (log), 4 or above (debug)."
 
 (defmacro eask--log-write-buffer (buffer file)
   "Write BUFFER to FILE."
-  `(let ((buffer-file-coding-system 'utf-8))
-     (write-region (with-current-buffer ,buffer (buffer-string)) nil
-                   (expand-file-name ,file log-dir))))
+  `(when (get-buffer-create ,buffer)
+     (let ((buffer-file-coding-system 'utf-8))
+       (write-region (with-current-buffer ,buffer (buffer-string)) nil
+                     (expand-file-name ,file log-dir)))))
 
 (add-hook 'kill-emacs-hook  ; Write log files
           (lambda (&rest _)
             (when eask-log-file
               (let ((log-dir (expand-file-name eask-log-path)))
                 (make-directory log-dir t)
-                (eask--log-write-buffer "*Messages*" "messages.log")))))
+                (eask--log-write-buffer "*Messages*" "messages.log")
+                (eask--log-write-buffer "*Warnings*" "warnings.log")
+                (eask--log-write-buffer "*Backtrace*" "backtrace.log")
+                (eask--log-write-buffer "*Compile-Log*" "compile-log.log")))))
 
 ;;
 ;;; File
