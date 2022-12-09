734592dafb45ca4d343e7bda7de944ed5b61c766
Jen-Chieh Shen <jcs090218@gmail.com>
2022-12-10 03:17:53 +0800

    Fix arguments

--- lisp/extern/compat.el
+++ lisp/extern/compat.el
@@ -6,6 +6,6 @@
   (defun directory-empty-p (dir)
     "..."
     (and (file-directory-p dir)
-         (null (directory-files dir nil directory-files-no-dot-files-regexp t 1)))))
+         (null (directory-files dir nil directory-files-no-dot-files-regexp t)))))
 
 ;;; extern/compat.el ends here
