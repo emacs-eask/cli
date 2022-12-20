177b19a0d383cd8863f7f365a123e75e9b5ac17c
Jen-Chieh <jcs090218@gmail.com>
2022-12-20 14:35:31 +0800

    feature: Adapat -a/--all option for archives command

--- lisp/core/archives.el
+++ lisp/core/archives.el
@@ -27,17 +27,33 @@
     (message (concat "  %-" eask--length-name "s  %-" eask--length-url "s  %-" eask--length-priority "s")
              name url (or priority 0))))
 
+(defun eask--print-archive-alist (alist)
+  "Print the archvie ALIST."
+  (let* ((names (mapcar #'car alist))
+         (eask--length-name (format "%s" (eask-seq-str-max names)))
+         (urls (mapcar #'cdr alist))
+         (eask--length-url (format "%s" (eask-seq-str-max urls)))
+         (priorities (mapcar #'cdr package-archive-priorities))
+         (eask--length-priority (format "%s" (eask-seq-str-max priorities))))
+    (mapc #'eask--print-archive alist)))
+
 (eask-start
-  (if package-archives
-      (progn
-        (let* ((names (mapcar #'car package-archives))
-               (eask--length-name (format "%s" (eask-seq-str-max names)))
-               (urls (mapcar #'cdr package-archives))
-               (eask--length-url (format "%s" (eask-seq-str-max urls)))
-               (priorities (mapcar #'cdr package-archive-priorities))
-               (eask--length-priority (format "%s" (eask-seq-str-max priorities))))
-          (mapc #'eask--print-archive package-archives))
-        (eask-info "(Total of %s archives)" (length package-archives)))
-    (eask-info "(No archive has been selected)")))
+  (cond
+   ((eask-all-p)
+    (eask-info "List all available archives:")
+    (eask-msg "")
+    (eask--print-archive-alist eask-source-mapping)
+    (eask-msg "")
+    (eask-info "(Total of %s archive%s available)" (length eask-source-mapping)
+               (eask--sinr eask-source-mapping "" "s")))
+   (package-archives
+    (eask-info "List archives that are currently using:")
+    (eask-msg "")
+    (eask--print-archive-alist package-archives)
+    (eask-msg "")
+    (eask-info "(Total of %s archive%s listed)" (length package-archives)
+               (eask--sinr package-archives "" "s")))
+   (t
+    (eask-info "(No archive has been selected)"))))
 
 ;;; core/archives.el ends here
