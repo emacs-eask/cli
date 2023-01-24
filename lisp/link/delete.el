a52bf9943fe3ee888421f76827aff391874d55ed
Jen-Chieh Shen <jcs090218@gmail.com>
2023-01-24 14:52:20 +0800

    chore: Improve help manual for `link` command (#110)
    * chore: Improve help manual for link comand
    
    * imp
    
    * Add instruction

--- lisp/link/delete.el
+++ lisp/link/delete.el
@@ -34,26 +34,46 @@
     (if (and source (file-symlink-p link))
         (progn
           (eask--delete-symlink link)
-          (eask-info "✓ Unlinked package `%s`" link)
+          (eask-info "✓ Package `%s` unlinked" name)
           t)
-      (eask-info "✗ Package `%s` not linked" name)
+      (eask-info "✗ No linked package name `%s`" name)
       nil)))
 
 (eask-start
-  (let ((names (eask-args))
-        (links (eask--links))
-        (deleted 0))
-    (cond ((zerop (length names))
-           (eask-info "✗ No package to unlink, please specify with the package name")
-           (eask-help "link/delete"))
-          (t
-           (dolist (name names)
-             (when (eask--delete-link name)
-               (cl-incf deleted)))
-           (eask-msg "")
-           (eask-info "(Total of %s package%s unlinked, %s skipped)"
-                      deleted
-                      (eask--sinr deleted "" "s")
-                      (- (length names) deleted))))))
+  (let* ((names (eask-args))
+         (links (eask--links))
+         (link-names (mapcar #'car links))
+         (deleted 0))
+    (cond
+     ;; Argument check.
+     ((zerop (length names))
+      (eask-info "✗ No package to unlink, please specify with the package name")
+      (eask-help "link/delete"))
+     ;; No link to delete
+     ((zerop (length links))
+      (eask-info "No links presented; tasks ended with no operation"))
+     ;; Link not found
+     ((cl-some (lambda (name)
+                 (not (member name link-names)))
+               names)
+      (eask-info "(No operation)")
+      (eask-msg "")
+      (eask-info "Arguments contain invalid link name:")
+      (eask-msg "")
+      (dolist (name names)
+        (unless (memq name link-names)
+          (eask-msg "  ✗ %s" name)))
+      (eask-msg "")
+      (eask-call "link/list"))
+     ;; Okay! Good to go!
+     (t
+      (dolist (name names)
+        (when (eask--delete-link name)
+          (cl-incf deleted)))
+      (eask-msg "")
+      (eask-info "(Total of %s package%s unlinked, %s skipped)"
+                 deleted
+                 (eask--sinr deleted "" "s")
+                 (- (length names) deleted))))))
 
 ;;; link/delete.el ends here
