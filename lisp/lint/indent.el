9aaaceef79a81a8713fcd20e4bebc5a045eff013
JenChieh <jcs090218@gmail.com>
2023-03-21 23:25:53 -0700

    fix(lint/indent): Load files before indent lint

--- lisp/lint/indent.el
+++ lisp/lint/indent.el
@@ -52,6 +52,8 @@
   (let* ((patterns (eask-args))
          (files (if patterns (eask-expand-file-specs (eask-args))
                   (eask-package-el-files))))
+    (eask-install-dependencies)
+    (ignore-errors (mapc #'load (eask-package-el-files)))
     (cond
      ;; Files found, do the action!
      (files
