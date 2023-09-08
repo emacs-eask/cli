f24791901ed5f2605d2999a509dbe93a19a34418
JenChieh <jcs090218@gmail.com>
2023-09-07 17:47:52 -0700

    fix(init): Improve conversion from Cask

--- lisp/init/cask.el
+++ lisp/init/cask.el
@@ -79,17 +79,20 @@ Optional argument CONTENTS is used for nested directives.  e.g. development."
       (setq deps (append deps (eask--cask-filter-contents 'depends-on (cdr dev-scope)))))
     deps))
 
-(defun eask--cask-no-reqs ()
-  "Return non-nil if no dependencies from Cask-file."
-  (and (not (eask--cask-reqs-no-emacs))
-       (not (eask--cask-reqs-dev))))
-
-(defun eask--cask-reqs-no-emacs ()
-  "Return dependencies from Cask-file but exclude Emacs."
+(defun eask--cask-remove-emacs-dep (list)
+  "Remove dependency Emacs from dependencies LIST."
   (cl-remove-if (lambda (req)
                   (when (string= (cadr req) "emacs")
                     (caddr req)))
-                (eask--cask-reqs)))
+                list))
+
+(defun eask--cask-reqs-no-emacs ()
+  "Return dependencies from Cask-file but exclude Emacs."
+  (eask--cask-remove-emacs-dep (eask--cask-reqs)))
+
+(defun eask--cask-reqs-dev-no-emacs ()
+  "Return development dependencies from Cask-file but exclude Emacs."
+  (eask--cask-remove-emacs-dep (eask--cask-reqs-dev)))
 
 (defun eask--cask-emacs-version ()
   "Return Emacs version from Cask-file."
@@ -171,13 +174,14 @@ Optional argument CONTENTS is used for nested directives.  e.g. development."
                    (insert "\n")
                    (insert "(script \"test\" \"echo \\\"Error: no test specified\\\" && exit 1\")\n")
 
-                   (dolist (source (eask--cask-sources))
+                   (when-let ((sources (eask--cask-sources)))
                      (insert "\n")
-                     (insert "(source '" (eask-2str (cadr source)) ")\n"))
+                     (dolist (source sources)
+                       (insert "(source '" (eask-2str (cadr source)) ")\n")))
 
                    (insert "\n")
                    (insert "(depends-on \"emacs\" \"" emacs-version "\")")
-                   (when (eask--cask-no-reqs)
+                   (unless (eask--cask-reqs-no-emacs)
                      (insert "\n"))  ; Make sure end line exists!
 
                    (when-let ((pkgs (eask--cask-reqs-no-emacs)))
@@ -186,7 +190,7 @@ Optional argument CONTENTS is used for nested directives.  e.g. development."
                        (let ((val (mapconcat #'eask-2str (cdr pkg) "\" \"")))
                          (insert "(depends-on \"" val "\")\n"))))
 
-                   (when-let ((pkgs (eask--cask-reqs-dev)))
+                   (when-let ((pkgs (eask--cask-reqs-dev-no-emacs)))
                      (insert "\n")
                      (insert "(development\n")
                      (dolist (pkg pkgs)
