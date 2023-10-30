bfa4d851c08b2ee671bd0817b19771e5fd4d5a97
JenChieh <jcs090218@gmail.com>
2023-10-29 18:38:47 -0700

    Update copyright

--- cmds/core/emacs.js
+++ cmds/core/emacs.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022-2023 Jen-Chieh Shen
+ * Copyright (C) 2022-2023 the Eask authors.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
