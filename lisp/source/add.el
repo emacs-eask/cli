e473afcff049274aa92b4a60b466d2f6763fa7e0
JenChieh <jcs090218@gmail.com>
2023-11-12 20:16:43 -0800

    fix: correct logic

--- lisp/source/add.el
+++ lisp/source/add.el
@@ -20,6 +20,10 @@
                           (locate-dominating-file dir "_prepare.el"))
         nil t))
 
+(defun eask--source-from-mapping (name url)
+  "Return t if NAME and URL matched our database."
+  (string= (eask-source-url name url) url))
+
 (defun eask--source-add (name url exists)
   "Add an archive source by NAME.
 
@@ -29,7 +33,7 @@ Arguments EXISTS is used to print the information."
   (let* ((style-sym (string-match "([ \t\n\r]*source[ \t\n\r]*[']+" (buffer-string)))
          (name-str (if style-sym (concat "'" name)
                      (concat "\"" name "\""))))
-    (if url
+    (if (and url (not (eask--source-from-mapping name url)))
         (insert "(source " name-str " \"" url "\")\n")
       (insert "(source " name-str ")\n"))
     (eask-info "(New source `%s' added and points to `%s')" name (or url (cdr exists)))))
@@ -45,17 +49,26 @@ The argument EXISTS is use to search for correct position to insert new source."
     (cond
      (exists
       (if (re-search-forward (concat "([ \t\n\r]*source[ \t\n\r]*['\"]+" name)  nil t)
-          (progn
-            (re-search-forward "[ \t\r\n]*" nil t)  ; Forward to non-space characters!
+          (let ((start (point))
+                (built-in (string= url (eask-source-url name url))))
+            (when (string= "\"" (string (char-after)))
+              (cl-incf start))
+            (re-search-forward "[ \t\r\n\"]*" nil t)  ; Forward to non-space characters!
+            (forward-char -1)
             (pcase (string (char-after))
               (")"
-               (insert " \"" url "\""))
+               (unless built-in
+                 (insert " \"" url "\"")))
               ("\""
-               (let ((old (thing-at-point 'string))
-                     (new (concat "\"" url "\"")))
-                 (delete-region (point) (+ (point) (length old)))
-                 (insert new))))
-            (eask-info "(Changed archive `%s's location from `%s' to `%s')"
+               (if built-in
+                   (delete-region start (save-window-excursion
+                                          (search-forward ")" nil t)
+                                          (1- (point))))
+                 (let ((old (thing-at-point 'string))
+                       (new (concat "\"" url "\"")))
+                   (delete-region (point) (+ (point) (length old)))
+                   (insert new)))))
+            (eask-info "(Changed archive `%s''s location from `%s' to `%s')"
                        name (cdr exists) url))
         (goto-char (point-max))
         (eask--source-add name url exists)))
@@ -72,16 +85,18 @@ The argument EXISTS is use to search for correct position to insert new source."
 Arguments NAME and URL are main arguments for this command."
   (if-let ((exists (assoc name package-archives))
            (old-url (cdr exists)))
-      (if (and (not (string= old-url url))
-               (yes-or-no-p
-                (format
-                 (concat
-                  "The archive `%s' is already exists and currently points  to `%s'.
+      (cond ((string= old-url url)
+             (eask-info "(Nothing has changed due to the URLs are the same)"))
+            ((yes-or-no-p
+              (format
+               (concat
+                "The archive `%s' is already exists and currently points  to `%s'.
 
 Do you want to overwrite it? ")
-                 name old-url)))
-          (eask--source-write name url exists)
-        (eask-info "(Nothing has changed due to the URLs are the same)"))
+               name old-url))
+             (eask--source-write name url exists))
+            (t
+             (eask-info "(Nothing has changed, aborted)")))
     (eask--source-write name url nil)))
 
 (eask-start
@@ -91,7 +106,10 @@ Do you want to overwrite it? ")
     (cond
      (url (eask--source-ask-if-overwrite name url))
      (t
-      (if-let ((archive (assoc (eask-intern name) eask-source-mapping)))
+      (if-let* ((archive (assoc (eask-intern name) eask-source-mapping))
+                (name (car archive))
+                (name (eask-2str name))
+                (url (eask-source-url name)))  ; Use the URL from our database!
           (eask--source-ask-if-overwrite name url)
         (eask-info "(Invalid source name, `%s')" name)
         (eask-help "source/add"))))))
