446d8ee2b75945ed389cfd6f0397cce8f5cb315a
JenChieh <jcs090218@gmail.com>
2023-11-17 21:09:42 -0800

    chore: Update copyright

--- webinstall/install.bat
+++ webinstall/install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:: Copyright 2023 the Eask authors.
+:: Copyright (C) 2023 the Eask authors.
 
 :: This program is free software; you can redistribute it and/or modify
 :: it under the terms of the GNU General Public License as published by
