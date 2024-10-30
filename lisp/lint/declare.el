ffecfd7cc88851871fe24ee67cda56d5bb737535
Josh B <30708316+joshbax189@users.noreply.github.com>
2024-10-30 19:18:35 +1300

    Fix: match behavior of declare linter to checkdoc (#272)
    * use eask-report so eask-strict-p flag is checked
    
    * use inhibit-message to hide output from check-declare-file
    
    * set allow-error=always in declare linter
    
    * use eask--silent macro instead

--- lisp/lint/declare.el
+++ lisp/lint/declare.el
@@ -25,6 +25,11 @@
 (defvar check-declare-warning-buffer)
 
 ;;
+;;; Flags
+
+(advice-add #'eask-allow-error-p :override #'always)
+
+;;
 ;;; Core
 
 (defun eask-lint-declare--file (filename)
@@ -34,10 +39,10 @@
          (errors))
     (eask-lint-first-newline)
     (eask-msg "`%s` with check-declare" (ansi-green file))
-    (setq errors (check-declare-file filename))
+    (setq errors (eask--silent (check-declare-file filename)))
     (if errors
         (with-current-buffer check-declare-warning-buffer
-          (eask-msg (buffer-string)))
+          (eask-report (string-remove-prefix "\n" (buffer-string))))
       (eask-msg "No issues found"))))
 
 (eask-start
