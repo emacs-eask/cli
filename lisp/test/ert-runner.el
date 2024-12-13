d9041eaa3d15c82d196e9264faf74660eaa117ff
JenChieh <jcs090218@gmail.com>
2024-12-13 10:58:43 -0800

    feat(test): Make ert-runner accepts files arguments

--- lisp/test/ert-runner.el
+++ lisp/test/ert-runner.el
@@ -4,7 +4,12 @@
 ;;
 ;; Command to run ert tests using ert-runner,
 ;;
-;;   $ eask ert-runner
+;;   $ eask ert-runner [files..]
+;;
+;;
+;;  Positionals:
+;;
+;;    [files..]     specify files to run ert tests
 ;;
 
 ;;; Code:
@@ -20,9 +25,25 @@
             (when (eask-reach-verbosity-p 'debug)
               (setq ert-runner-verbose t))))
 
+(defun eask-test-ert-runner--run (fnc &rest args)
+  "Run around function `ert-runner/run'.
+
+Arguments FNC and ARGS are used for advice `:around'.
+
+Handle the argument ARGS when command arguments are specified."
+  (let* ((patterns (eask-args))
+         (files (eask-expand-file-specs patterns)))
+    (setq args files))
+  (apply fnc args))
+
+(advice-add 'ert-runner/run :around #'eask-test-ert-runner--run)
+
 (eask-start
+  ;; Preparation
   (eask-archive-install-packages '("gnu" "melpa")
                                  'ert-runner)
+
+  ;; Start Testing
   (require 'ert-runner))
 
 ;;; test/ert-runner.el ends here
