d40a3b8675006d6d5fdc50dfe73f176f49fc90cc
JenChieh <jcs090218@gmail.com>
2025-03-14 13:57:10 -0700

    feat(docs): Add subtitle under title

--- docs/static/main.js
+++ docs/static/main.js
@@ -1,15 +1,16 @@
 
+const quote = "Learn once, run anywhere";
 
 window.addEventListener('load', function () {
+  var subtitle = document.getElementsByClassName("_subtitle");
+
+  // In case adding more than once!
+  if (subtitle.length != 0)
+    return;
+
   const titles = document.getElementsByClassName("gdoc-brand__title");
 
   const title = titles[0]
 
-  const subtitle = document.createElement('div');
-
-  subtitle.classList.add("_subtitle");
-
-  title.appendChild(subtitle);
-
-  subtitle.innerHTML = "Learn once, run anywhere";
+  title.innerHTML += '<div class="_subtitle">' + quote + '</div>';
 });
