34c79668719afc62d762d86306886517631e80de
JenChieh <jcs090218@gmail.com>
2025-03-14 21:51:56 -0700

    docs: Avoid identifier has already been declared error

--- docs/static/main.js
+++ docs/static/main.js
@@ -1,12 +1,12 @@
-
-const quote = "Learn once, run anywhere";
-
 window.addEventListener('load', function () {
   var subtitle = document.getElementsByClassName("_subtitle");
 
   // In case adding more than once!
   if (subtitle.length != 0)
     return;
+
+  // Place it here to avoid error: `Identifier 'xxx' has already been declared`.
+  const quote = "Learn once, run anywhere";
 
   const titles = document.getElementsByClassName("gdoc-brand__title");
 
