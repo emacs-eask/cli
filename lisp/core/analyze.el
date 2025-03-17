78b5f9272996c81313b818ea79106bc7ff35507c
Josh Bax <joshbax189@gmail.com>
2025-03-17 14:50:42 -0700

    Fix: bad help file link for analyze command

--- lisp/core/analyze.el
+++ lisp/core/analyze.el
@@ -152,6 +152,6 @@ Argument LEVEL and MSG are data from the debug log signal."
    ;; Default, print help!
    (t
     (eask-info "(No Eask-files have been checked)")
-    (eask-help "checker/analyze"))))
+    (eask-help "core/analyze"))))
 
 ;;; core/analyze.el ends here
