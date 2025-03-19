274ee3cfd7f8ee1412a33f4bbd2b0d8f1ba8361a
Josh Bax <joshbax189@gmail.com>
2025-03-18 17:53:54 -0700

    Port analyze tests

---
+++ test-js/dsl/check-dsl.el
@@ -0,0 +1,37 @@
+;;; check-dsl.el --- Test for DSL  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2025  the Eask authors.
+;; Created date 2022-04-19 15:16:50
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/emacs-eask/check-dsl
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3") (dash "0.1"))
+;; Keywords: dsl
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Test for DSL.
+;;
+
+;;; Code:
+
+
+
+(provide 'check-dsl)
+;;; check-dsl.el ends here
