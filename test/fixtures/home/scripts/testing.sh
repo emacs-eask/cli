201ac545d494db118d5ef75d3fbf05cc6bee56dd
JenChieh <jcs090218@gmail.com>
2025-03-18 21:59:49 -0700

    chore: Add copyright info

--- test/fixtures/home/scripts/testing.sh
+++ test/fixtures/home/scripts/testing.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+# Copyright (C) 2025 the Eask authors.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## Commentary:
+#
 # Test reporting functions
 # This file will be sourced by other shell test scripts.
 # Expects to run with set -e
 
-# TODO note that eask output is always to stderr
+# TODO: note that eask output is always to stderr
 
 # Run command and exit with the same status
 # All args interpreted as program to run
