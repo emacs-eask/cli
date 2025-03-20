1a1c302cef620a4274980cd02a9fd86509078cb0
Josh Bax <joshbax189@gmail.com>
2025-03-19 18:43:02 -0700

    Add emacsVersion helper
    Fix return type of emacsVersion()

--- test-js/helpers.js
+++ test-js/helpers.js
@@ -1,3 +1,6 @@
+const util = require("node:util");
+const exec = util.promisify(require("node:child_process").exec);
+
 /**
  * As for jest.test but skips running if the ALLOW_UNSAFE env var is set.
  * @param {string} name
@@ -12,4 +15,18 @@ function testUnsafe(name, fn, timeout) {
   }
 }
 
-module.exports = { testUnsafe };
+/**
+ * The version string for system emacs, e.g. "30.0.50".
+ * You can compare lexicographically, e.g. if ((await emacsVersion()) > "27") { ... }
+ * @returns {Promise.<string>} emacs version string.
+ */
+async function emacsVersion() {
+  const text = await exec("emacs --version");
+  const version = text.stdout.match("Emacs ([^ ]+)")?.[1];
+  if (!version) {
+    throw Error("Couldn't extract Emacs version. Text was:\n" + text);
+  }
+  return version;
+}
+
+module.exports = { testUnsafe, emacsVersion };
