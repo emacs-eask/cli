d137a5501a3e0930c6948c52898a005abaff03aa
JenChieh <jcs090218@gmail.com>
2025-03-23 10:15:12 -0700

    docs: Use built in subtitle params

--- docs/static/main.js
+++ docs/static/main.js
@@ -1,16 +1,3 @@
 window.addEventListener('load', function () {
-  var subtitle = document.getElementsByClassName("_subtitle");
-
-  // In case adding more than once!
-  if (subtitle.length != 0)
-    return;
-
-  // Place it here to avoid error: `Identifier 'xxx' has already been declared`.
-  const quote = "Learn once, run anywhere";
-
-  const titles = document.getElementsByClassName("gdoc-brand__title");
-
-  const title = titles[0]
-
-  title.innerHTML += '<div class="_subtitle">' + quote + '</div>';
+	// Code removed due to https://github.com/thegeeklab/hugo-geekdoc/pull/1003
 });
