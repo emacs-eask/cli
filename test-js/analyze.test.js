b8c49796d13648a569aab61a3b27a2792f81ab93
Josh Bax <joshbax189@gmail.com>
2025-03-24 21:19:20 -0700

    Add examples of snapshots to analyze.test.js

--- test-js/analyze.test.js
+++ test-js/analyze.test.js
@@ -1,30 +1,44 @@
 const { TestContext } = require("./helpers");
 
 describe("analyze", () => {
-  describe("dsl", () => {
-    const cwd = "./test-js/dsl";
-    const ctx = new TestContext(cwd);
+  describe("in ./dsl", () => {
+    const ctx = new TestContext("./test-js/dsl");
 
     it("handles plain text", async () => {
       await ctx.runEask("analyze");
       await ctx.runEask("analyze Eask");
     });
 
-    it("handles json", async () => {
-      await ctx.runEask("analyze --json");
+    it("handles json option", async () => {
+      const { stderr } = await ctx.runEask("analyze --json");
       await ctx.runEask("analyze Eask --json");
+
+      // try to parse output, errors if not valid
+      JSON.parse(stderr);
+    });
+
+    it("matches snapshot", async () => {
+      const res = await ctx.runEask("analyze");
+      const resClean = res.sanitized().raw();
+      expect(resClean).toMatchSnapshot();
+    });
+
+    it("should report multiple definitions", async () => {
+      const { stderr } = await ctx.runEask("analyze");
+      // expect this substring
+      expect(stderr).toMatch("Multiple definition of `package'");
     });
   });
 
-  describe("error", () => {
+  describe("in ./empty", () => {
     const ctx = new TestContext("./test-js/empty");
 
-    it("errors in an empty dir", async () => {
+    it("should error", async () => {
       await expect(ctx.runEask("analyze")).rejects.toThrow();
     });
   });
 
-  describe("metadata", () => {
+  describe("in ./metadata", () => {
     const ctx = new TestContext("./test-js/metadata");
 
     it("handles plain text", async () => {
