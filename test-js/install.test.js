8f8b559478940a925f082cac28e41ce36aa22c5d
Josh Bax <joshbax189@gmail.com>
2025-03-25 14:54:11 -0700

    Check result of commands in install.test.js

--- test-js/install.test.js
+++ test-js/install.test.js
@@ -1,37 +1,45 @@
 const { TestContext } = require("./helpers");
 
 describe("install and uninstall", () => {
-  describe("in mini.pkg.1", () => {
+  describe("in ./install", () => {
     const ctx = new TestContext("./test-js/install/");
+    const packageName = "mini.pkg.1";
 
-    // beforeAll(async () => await exec("eask clean all", { cwd }));
-
-    // TODO also teardown?
-    afterAll(async () => await ctx.runEask("clean all"));
-    afterAll(() => ctx.cleanUp());
-
-    // TODO this can take a really really long time!
-    // TODO all later commands depend on this?
-    test("installs dependencies", async () => {
+    beforeAll(async () => {
+      await ctx.runEask("clean all");
       await ctx.runEask("install-deps");
     });
 
+    afterAll(() => ctx.cleanUp());
+
     it("installs project package", async () => {
-      await ctx.runEask("package");
-      await ctx.runEask("install");
+      await ctx.runEask("package"); // creates dist/<pkg>.tar
+      await ctx.runEask("install"); // installs dependencies and generated package
+      const { stderr } = await ctx.runEask("list");
+      expect(stderr).toMatch(packageName);
     });
 
     it("installs specific packages", async () => {
-      await ctx.runEask("install beacon company-fuzzy transwin");
+      const { stderr } = await ctx.runEask(
+        "install beacon company-fuzzy transwin",
+      );
+      expect(stderr).toMatch("beacon");
+      expect(stderr).toMatch("company-fuzzy");
+      expect(stderr).toMatch("transwin");
     });
 
-    // TODO stateful
     it("uninstalls specific packages", async () => {
+      await ctx.runEask("install beacon transwin");
       await ctx.runEask("uninstall beacon transwin");
+      const { stderr } = await ctx.runEask("list");
+      expect(stderr).not.toMatch("beacon");
+      expect(stderr).not.toMatch("transwin");
     });
 
     it("uninstalls project package", async () => {
       await ctx.runEask("uninstall");
+      const { stderr } = await ctx.runEask("list");
+      expect(stderr).not.toMatch(packageName);
     });
   });
 
