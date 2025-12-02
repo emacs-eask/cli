e15424c860d717c8b0093c92d8169808749e0723
Jen-Chieh Shen <jcs090218@gmail.com>
2025-12-02 18:52:12 +0800

    test(docker): Add test for docker image name

--- test/jest/docker.test.js
+++ test/jest/docker.test.js
@@ -9,4 +9,9 @@ describe("docker", () => {
     // this can take a long time to pull and build the image
     await ctx.runEask("docker 27.1 info", { timeout: 40000 });
   });
+
+  test("eask docker silex/emacs:27.1-eask info", async () => {
+    // this can take a long time to pull and build the image
+    await ctx.runEask("docker silex/emacs:27.1-eask info", { timeout: 40000 });
+  });
 });
