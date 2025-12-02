b633f889d24ebaa28b199a0b9d7384c7df6f7ea6
Jen-Chieh Shen <jcs090218@gmail.com>
2025-12-02 06:00:26 -0800

    feat: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Development shell for Emacs + Node.js with Eask";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      # Define the systems you want to support
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+      {
+        # Generate devShells for each system
+        devShells = nixpkgs.lib.genAttrs systems (system:
+          let
+            # Get the nixpkgs set for the current system
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+            {
+              # Define the default development shell for the system
+              default = pkgs.mkShell {
+                buildInputs = [
+                  pkgs.emacs
+                  pkgs.nodejs
+                ];
+
+                shellHook = ''
+                    echo "Welcome to the Emacs development shell with Eask!"
+                    npm install --include=dev
+                    eask --version
+                    '';
+              };
+            }
+        );
+      };
+}
